f94d6b0122e894fcbe60e08ed605af0d9d487b60
redwolfwind <88865482+redwolfwind@users.noreply.github.com>
2023-10-13 01:08:16 +0000

    Second commit

--- static/blink.js
+++ static/blink.js
@@ -10,3 +10,27 @@ setInterval(function(){
 		eyes.style.display = "block";
 	}
 }, 500)
+
+
+var open = true;
+var mouthImg = document.getElementById("closed");
+var move;
+
+function StartAnimation(){
+	move = setInterval(function(){
+		if(open){
+			open = false;
+			mouthImg.src = "../static/mouthclosed.png"
+		}
+		else{
+			open = true;
+			mouthImg.src = "../static/mouthopen.png"
+		}
+	}, 200)
+}
+function EndAnimation(){
+	running = false;
+	clearInterval(move);
+	open = false;
+	mouthImg.src = "../static/mouthclosed.png"
+}
